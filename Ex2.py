aaa7863d9a8ead3c262fe0b81a1d551c79d75d7c
aminmansuri123 <mansuri.amin1@gmail.com>
2026-01-31 10:19:25 +0530

    Initial commit

---
+++ Ex2.py
@@ -0,0 +1,4 @@
+inp1 = input("Enter your First Name ")
+inp2 = input("Enter your Last Name ")
+
+print("Hello, ", inp1 +" "+inp2+"!","Welcome to the Python Program.")
---
+++ Exc1.py
@@ -0,0 +1,7 @@
+inp1 = int(input("Enter the First Number "))
+inp2 = int(input("Enter the Second Number "))
+
+print("Addition:", inp1 + inp2)
+print("Subtraction:", inp1 - inp2)
+print("Multiplication:", inp1 * inp2)
+print("Division:", inp1 / inp2)
