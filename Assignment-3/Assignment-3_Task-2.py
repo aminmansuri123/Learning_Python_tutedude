495eb2012be474866a24628202d0e16597e7accd
aminmansuri123 <mansuri.amin1@gmail.com>
2026-02-01 18:17:47 +0530

    Initial add

---
+++ Assignment-3/Assignment-3_Task-2.py
@@ -0,0 +1,11 @@
+import math
+
+user_input = int(input("Write a number: "))
+
+square_root = math.sqrt(user_input)
+natural_log = math.log(user_input)
+Sine_value = math.sin(user_input)
+
+print(f"Square Root: {square_root}")
+print(f"Logarithm: {natural_log}")
+print(f"Sine: {Sine_value}")
